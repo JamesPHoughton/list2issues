da1c1a021233b1c6a7ea8688339f5b47d20a4160
James Houghton <James.P.Houghton@gmail.com>
2020-05-28 16:53:14 -0400

    debug

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const { Toolkit } = require('actions-toolkit')
 // Run your GitHub Action!
 Toolkit.run(async tools => {
 
-  const listFile = tools.inputs.filename
+  const listFile = tools.inputs.filename;
 
   // Get the file
-  tools.log.debug('Reading from file', listFile)
-  const text = await tools.readFile(listFile)
+  tools.log.debug('Reading from file', listFile);
+  const text = await tools.readFile(listFile);
 
   // break the file apart into milestones
-  const milestones = text.split(/.*(?=[\r\n]\#\s)/).map(x => x.trim())
+  const milestones = text.split(/.*(?=[\r\n]\#\s)/).map(x => x.trim());
 
   const result = await Promise.all(milestones.map( async ms => {
     const ms_title = ms.split(/[\r\n]/)[0].slice(1).trim()
@@ -25,7 +25,7 @@ Toolkit.run(async tools => {
     tools.log.success(`Created Milestone ${newMilestone.data.title}#${newMilestone.data.number}`)
 
     // break milestone apart into issues
-    const issues = ms.split(/[\r\n]\#\#\s/).slice(1)
+    const issues = ms.split(/[\r\n]\#\#\s/).slice(1);
     const iss_res = await Promise.all(issues.map( async iss => {
       const lines = iss.split(/[\n\r]/)
       const iss_title = lines[0]
@@ -39,8 +39,8 @@ Toolkit.run(async tools => {
       })
       tools.log.success(`Created issue ${newIssue.data.title}#${newIssue.data.number}: ${newIssue.data.html_url}`)
 
-    });
-  }
+    }));
+  }));
 
   tools.exit.success('We did it!')
 
